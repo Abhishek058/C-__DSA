64bd41f7757e68b4d5b4b5e6d3f44a2b6d8574d7
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-04 18:09:10 +0530

    updated

--- Queue/KQueue.cpp
+++ Queue/KQueue.cpp
@@ -1,92 +1,99 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Kqueue
-{
-public:
-    int n;
-    int k;
-    int *front;
-    int *rear;
-    int *arr;
-    int freeSpot;
-    int *next;
+class kQueue {
 
-public:
-    Kqueue(int n, int k)
-    {
-        this->n = n;
-        this->k = k;
-        front = new int[k];
-        rear = new int[k];
-        for (int i = 0; i < k; i++)
-        {
-            front[i] = -1;
-            rear[i] = -1;
-        }
-        next = new int[n];
-        for (int i = 0; i < k; i++)
-        {
-            next[i] = -1;
-        }
+    public:
+        int n;
+        int k;
+        int *front;
+        int *rear;
+        int *arr;
+        int freeSpot;
+        int *next;
 
-        next[n - 1] = -1;
+    public:
+        kQueue(int n, int k) {
+            this -> n = n;
+            this -> k = k;
+            front = new int[k];
+            rear = new int[k];
+            for(int i=0; i<k; i++) {
+                front[i] = -1;
+                rear[i] = -1;
+            }
 
-        arr = new int[n];
+            next = new int[n];
+            for(int i=0; i<n; i++) {
+                next[i] = i+1;
+            }
+            next[n-1] = -1;
+            arr = new int[n];
+            freeSpot = 0;
+        }    
 
-        freeSpot = 0;
-    }
+        void enqueue(int data, int qn) {
 
-    void enqueue(int data, int qn)
-    {
-        if (freeSpot == -1)
-        {
-            cout << "No Empty space is present" << endl;
-            return;
-        }
-        int index = freeSpot;
+            //overflow
+            if( freeSpot == -1) {
+                cout << "No Empty space is present" << endl;
+                return;
+            }
 
-        freeSpot = next[index];
+            //find first free index
+            int index = freeSpot;
 
-        if (front[qn - 1] == -1)
-        {
-            front[qn - 1] = index;
-        }
-        else
-        {
-            next[rear[qn - 1]] = index;
-        }
+            //update freespot
+            freeSpot = next[index];
 
-        next[index] = -1;
+            //check whther first element
+            if(front[qn-1] == -1){
+                front[qn-1] = index;
+            }
+            else{
+                //link new element to the prev element
+                next[rear[qn-1]] = index;
+            }
 
-        rear[qn - 1] = index;
+            //update next
+            next[index] = -1;
 
-        arr[index] = data;
-    }
+            //update rear
+            rear[qn-1] = index;
 
-    int dequeue(int qn)
-    {
-        if (front[qn - 1] == -1)
-        {
-            cout << "Queue UnderFlow " << endl;
-            return -1;
+            //push element
+            arr[index] = data;
         }
 
-        int index = front[qn - 1];
-        front[qn - 1] = next[index];
-        next[index] = freeSpot;
-        freeSpot = index;
-        return arr[index];
-    }
+        int dequeue(int qn) {
+            //underflow 
+            if(front[qn-1] == -1)
+            {
+                cout << "Queue UnderFlow " << endl;
+                return -1;
+            }
+
+            //find index to pop
+            int index = front[qn-1];
+
+            //front ko aage badhao
+            front[qn-1] = next[index];
+
+            //freeSlots ko manage karo
+            next[index] = freeSpot;
+            freeSpot = index;
+            return arr[index];
+        }
+
 };
 
-int main()
-{
-    Kqueue q(10, 3);
+int main() {
+
+    kQueue q(10, 3);
     q.enqueue(10, 1);
-    q.enqueue(15, 1);
+    q.enqueue(15,1);
     q.enqueue(20, 2);
-    q.enqueue(25, 1);
+    q.enqueue(25,1);
 
     cout << q.dequeue(1) << endl;
     cout << q.dequeue(2) << endl;
@@ -94,5 +101,6 @@ int main()
     cout << q.dequeue(1) << endl;
 
     cout << q.dequeue(1) << endl;
+
     return 0;
 }
