b9a84c06151ca7be1c1cd29f40473b2c902c23c6
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-16 17:04:23 +0530

    two function added to add values

--- Trees/BinarySearchTree.cpp
+++ Trees/BinarySearchTree.cpp
@@ -1,21 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node {
-    public:
-        int data;
-        Node* right;
-        Node* left;
+class Node
+{
+public:
+    int data;
+    Node *right;
+    Node *left;
 
-        Node(int d){
-            this->data = d;
-            this->left = NULL;
-            this->right = NULL;
-        }
+    Node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
 };
+
+Node *insertIntoBST(Node *&root, int d)
+{
+    if (root == NULL)
+    {
+        root = new Node(d);
+        return root;
+    }
+    if (d > root->data)
+    {
+        root->right = insertIntoBST(root->right, d);
+    }
+    else
+    {
+        root->left = insertIntoBST(root->left, d);
+    }
+
+    return root;
+}
+
+void takeInput(Node *&root)
+{
+    int data;
+    cin >> data;
+
+    while (data != -1)
+    {
+        insertIntoBST(root, data);
+        cin >> data;
+    }
+}
 
 int main()
 {
-    
+    Node *root = NULL;
+    cout << "Enter data in tree: ";
+    takeInput(root);
     return 0;
 }
