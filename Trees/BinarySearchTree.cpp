ff22b3ed230bb79c61d0861f1069f334c4be586e
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-17 19:49:21 +0530

    node Deletinon function added

--- Trees/BinarySearchTree.cpp
+++ Trees/BinarySearchTree.cpp
@@ -66,6 +66,46 @@ Node* maxVal(Node* root){
     return temp;
 }
 
+Node* deleteFromBST(Node* root, int val){
+    if(root == NULL){
+        return root;
+    }
+    if(root->data == val){
+
+        //0 child
+        if(root->left == NULL && root->right == NULL){
+            delete root;
+            return NULL;
+        }
+        //1 child
+        if(root->left != NULL && root->right == NULL){
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        if(root->left == NULL && root->right != NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+        //2 child
+        if(root->left != NULL && root->right != NULL){
+            int mini = minVal(root->right)->data;
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+    }
+    else if(root->data > val){
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+    else{
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
+
 void takeInput(Node *&root)
 {
     int data;
