cd027aed12fc1140f14a4d680bd6cf48a3726923
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2022-04-17 12:29:12 +0530

    max and min function added

--- Trees/BinarySearchTree.cpp
+++ Trees/BinarySearchTree.cpp
@@ -48,6 +48,24 @@ void inOrder(Node *root)
     inOrder(root->right);
 }
 
+Node* minVal(Node* root){
+    Node* temp = root;
+
+    while(temp->left != NULL){
+        temp = temp->left;
+    }
+    return temp;
+}
+
+Node* maxVal(Node* root){
+    Node* temp = root;
+
+    while(temp->right != NULL){
+        temp = temp->right;
+    }
+    return temp;
+}
+
 void takeInput(Node *&root)
 {
     int data;
@@ -67,5 +85,8 @@ int main()
     takeInput(root);
     cout<<"Printing tree: "<<endl;
     inOrder(root);
+    cout<<endl;
+    cout<<"Minimum value: "<<minVal(root)->data<<endl;
+    cout<<"Maximum value: "<<maxVal(root)->data<<endl;
     return 0;
 }
