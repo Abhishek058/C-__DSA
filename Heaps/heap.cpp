f39b60a6582f64e19776951f0baf1117c7e4877b
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-04-26 18:18:56 +0530

    Print function for heap added

--- Heaps/heap.cpp
+++ Heaps/heap.cpp
@@ -1,31 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Heap{
-    public:
+class Heap
+{
+public:
     int arr[100];
     int size = 0;
 
-    void insert(int val){
+    void insert(int val)
+    {
         size++;
         int index = size;
         arr[index] = val;
 
-        while(index > 1){
-            int parent = index/2;
-            if(arr[parent] < arr[index]){
+        while (index > 1)
+        {
+            int parent = index / 2;
+            if (arr[parent] < arr[index])
+            {
                 swap(arr[parent], arr[index]);
                 index = parent;
             }
-            else{
-                return ;
+            else
+            {
+                return;
             }
         }
+    }
+    void print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
 };
 
 int main()
 {
-    
+
     return 0;
 }
