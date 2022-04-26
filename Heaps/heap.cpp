e4e3cb9a1fa5c1810c6287ff1be5fee64911d533
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-04-26 18:17:08 +0530

    heap class added | insertion code updated

--- Heaps/heap.cpp
+++ Heaps/heap.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
 using namespace std;
 
+class Heap{
+    public:
+    int arr[100];
+    int size = 0;
+
+    void insert(int val){
+        size++;
+        int index = size;
+        arr[index] = val;
+
+        while(index > 1){
+            int parent = index/2;
+            if(arr[parent] < arr[index]){
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else{
+                return ;
+            }
+        }
+    }
+};
+
 int main()
 {
     
