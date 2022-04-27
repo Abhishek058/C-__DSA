4e24c2ec0314a30714cc141b34c936b5448a2fcc
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-04-27 07:42:44 +0530

    bug fixed in heapify function

--- Heaps/heap.cpp
+++ Heaps/heap.cpp
@@ -84,11 +84,11 @@ void Heapify(int arr[], int n, int i)
     int left = 2 * i;
     int right = 2 * i + 1;
 
-    if (left < n && arr[largest] < arr[left])
+    if (left <= n && arr[largest] < arr[left])
     {
         largest = left;
     }
-    if (right < n && arr[largest] < arr[right])
+    if (right <= n && arr[largest] < arr[right])
     {
         largest = right;
     }
