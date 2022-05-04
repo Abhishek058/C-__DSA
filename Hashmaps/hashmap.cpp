bb4e7e516d25aeaa75ff1f32760179ed51ad5806
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-04 16:58:11 +0530

    insertion methods aded

--- Hashmaps/hashmap.cpp
+++ Hashmaps/hashmap.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
+#include<map>
+#include<unordered_map>
 using namespace std;
 
 int main()
 {
-    
+    unordered_map<string, int> m;
+
+    //1st way of insertion
+    pair<string, int> p = make_pair("abhishek", 3);
+    m.insert(p);
+
+    //2nd way of insertion
+    pair<string, int> pair2("is", 2);
+    m.insert(pair2);
+
+    //3rd way of insertion
+    m["my"] = 1; //initialization
+    m["my"] = 3; //updation
+
+    cout<<m["my"];
+
+
     return 0;
 }
