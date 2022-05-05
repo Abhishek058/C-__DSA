8d4d8196d73204a4f71b4aac7e5e3287df5516f3
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-05 18:20:15 +0530

    insertion code written and passed the word 'abcd'

--- Tries/tries.cpp
+++ Tries/tries.cpp
@@ -27,10 +27,37 @@ public:
         root = new trieNode('\0');
     }
 
-    
+    void insertUntil(trieNode *root, string word)
+    {
+        if (word.length() == 0)
+        {
+            root->isTerminal = true;
+            return ;
+        }
+
+        int index = word[0] - 'A';
+        trieNode *child;
+
+        if (root->children[index] != NULL)
+        {
+            child = root->children[index];
+        }
+        else
+        {
+            child = new trieNode(word[0]);
+            root->children[index] = child;
+        }
+        insertUntil(child, word.substr(1));
+    }
+
+    void insertWord(string word)
+    {
+        insertUntil(root, word);
+    }
 };
 int main()
 {
-    
+    trie *t = new trie();
+    t->insertWord("abcd");
     return 0;
 }
