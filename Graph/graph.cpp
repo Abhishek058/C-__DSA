5c3a0189e9476571bdcd7a9cc72044a99a4236a4
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-15 15:48:41 +0530

    graph class created and adding and printing function added

--- Graph/graph.cpp
+++ Graph/graph.cpp
@@ -1,8 +1,42 @@
-#include<iostream>
+#include <iostream>
+#include <list>
+#include <unordered_map>
 using namespace std;
+
+class graph
+{
+public:
+    unordered_map<int, list<int>> adj;
+
+    void addEdge(int u, int v, bool direction)
+    {
+        // direction = 0 -> undirected graph
+        // direction = 1 -> directed graph
+
+        // create an edge from u to v
+        adj[u].push_back(v);
+
+        if (direction == 0)
+        {
+            adj[v].push_back(u);
+        }
+    }
+    void printList()
+    {
+        for (auto i : adj)
+        {
+            cout << i.first << "->";
+            for (auto j : i.second)
+            {
+                cout << j << ", ";
+            }
+            cout << endl;
+        }
+    }
+};
 
 int main()
 {
-    
+
     return 0;
 }
