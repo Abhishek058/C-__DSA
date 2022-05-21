df6b611e00b9ea113ccad5700285bf0cd63da79f
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-05-21 22:59:07 +0530

    new file added with graph class and adding edge function

---
+++ Graph/Shortest_path_DAG.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph
+{
+public:
+    unordered_map<int, list<pair<int, int>>> adj;
+
+    void addEdge(int u, int v, int weight)
+    {
+        pair<int, int> p = make_pair(v, weight);
+        adj[u].push_back(p);
+    }
+};
+
+int main()
+{
+    Graph g;
+    g.addEdge(0, 1, 5);
+    g.addEdge(0, 2, 3);
+    g.addEdge(1, 2, 2);
+    g.addEdge(1, 3, 6);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 4, 4);
+    g.addEdge(2, 5, 2);
+    g.addEdge(3, 4, -1);
+    g.addEdge(4, 5, -2);
+    return 0;
+}
